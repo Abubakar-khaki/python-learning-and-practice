f7e4d3728c0db10af9e96a3cff91d7236e4e4766
ifskhaki <ifskhaki@gmail.com>
2026-01-30 21:02:12 +0500

    Add day 3 assignment and comparison operators

---
+++ day 3 (2) compareizen operator.py
@@ -0,0 +1,18 @@
+# camparison operator ==, !=, <, >, <=, >=
+a = 10
+b = 20
+c = 5 + 5
+print("a == b:",a == b) # == use to check equality
+print("a != b:", a != b) # != use for check not equality
+print("a < b:", a < b) # < use to check less then
+print("a > b:",a > b) # > use to check greater then 
+print("a <= b:", a<= b) # <= use to check less then or equal
+print("a >= c:", a >= c) # >= use to check greater then or equal
+
+
+# example of comparison operator in if else
+age = 19 # age variable
+if age >= 18: # check if age is greater than or equal to 18
+    print("You are eligible to vote.") # if condition is true
+else: # else condition
+    print("You are not eligible to vote.") # if condition is false
