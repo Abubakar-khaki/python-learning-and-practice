4208721253dab0fd8dafa6228d65f161c7ceda67
ifskhaki <ifskhaki@gmail.com>
2026-02-06 20:43:15 +0500

    Add day 5 data input

---
+++ day 5/data input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ") 
+age = int(input("Enter your age: "))
+gender = (input("choose your gender. enter M for male and F for female: "))
+game = input("Enter your favorite game: ")
+
+
+# if gender == "m":
+#     print("male ")
+# elif gender == "f":
+#     print("female")
+# else:
+#     print("Invalid input")
