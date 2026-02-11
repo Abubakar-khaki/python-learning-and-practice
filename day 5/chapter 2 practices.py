6623df552b923b8fb18e4d7804fec14c1f05e906
ifskhaki <ifskhaki@gmail.com>
2026-02-11 10:22:03 +0500

    Add day 5 chapter 2 practice exercises

---
+++ day 5/chapter 2 practices.py
@@ -0,0 +1,28 @@
+#Q1. write a program for sum two numers and print result
+a = 5
+b = 10
+print ("ans 1. a + b =:", a + b)
+
+#Q2. write a program for find reminder a divied by b
+a = 36
+b = 5
+print ("ans 2. ", a, "divided by", b, "gives a reminder of:", a % b)
+
+#Q3. check the type of variable assigned using input() function
+a = input("enter your value of a: ")
+print("ans 3. ",type(a))
+
+#Q4. useing comparison operator to find out whether a is greater than b or not
+a = 34
+b = 80
+print("ans 4. is a greater than b or not?", a > b)
+
+#Q5. write a program to find average of two numbers entered by user
+a = int(input("enter your first number: "))
+b = int(input("enter your second number: "))   
+print("ans 5. average of two numbers is:", (a + b) / 2)    
+
+#Q6. write a program to calculate square of a number entered by user
+a = int(input("enter you number: "))
+print("ans 6. square of the ", a ," is: ", a ** 2)
+print("ans 6. square of the ", a ," is: ", a ** a) # also useing this way
--- day 5/data input.py
+++ day 5/data input.py
@@ -3,10 +3,17 @@ age = int(input("Enter your age: "))
 gender = (input("choose your gender. enter M for male and F for female: "))
 game = input("Enter your favorite game: ")
 
+print("Your name is:", name)
+print("Your age is:", age)
+if gender == "M" or gender == "m":
+    print("Your gender is male.") 
+elif gender == "F" or gender == "f":
+    print("Your gender is female.")
+else:
+    print("Invalid input for gender.")
+print("Your favorite game is :", game)
 
-# if gender == "m":
-#     print("male ")
-# elif gender == "f":
-#     print("female")
-# else:
-#     print("Invalid input")
+a = input("enter value:")
+b = input("enter value:")
+sum = int(a) + float(b)
+print("The sum of", a, "and", b, "is:", sum) 
