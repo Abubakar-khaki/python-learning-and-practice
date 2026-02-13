3b5b662a767c131be9c9657b870bf0831480a32f
ifskhaki <ifskhaki@gmail.com>
2026-02-13 22:19:34 +0500

    day 7

---
+++ day 7/escape sequence.py
@@ -0,0 +1,16 @@
+# type of escape sequence
+print("abubakar \n khaki")# \n use for new line
+# in result we see that after \n the text is printed in new line
+print("abubakar \t khaki") # \t use for tab space
+# in result we see that after \t there is a tab space between abubakar and khaki
+print("12345abubakar\rkhaki") # \r if use where \r after this text move to frant of line and replace text
+print("abubakar\bkhaki") # \b use for backspace  
+print("abubakar\fkhaki") # \f use for form feed and it is used to move the cursor to the next line and also to the beginning of the line    
+print("abubakar\\khaki") # \\ use for backslash
+print("abubakar \'khaki\'") # \' use for single quote
+print("abubakar \"khaki\"") # \" use for double quote
+
+# differance in \n and \f is \n use for new line and it is used to
+#  move the cursor to the next line but it does not move the cursor
+#  to the beginning of the line but \f use for form feed and it is used
+#  to move the cursor to the next line and also to the beginning of the line
